441fd3a93811ac9409ed6f97f637547840ec2055
Srinivas Seedaragaddi <srinivasvkumar@gmail.com>
2023-11-02 12:12:21 +0530

    Day17 init

--- Projects/Day17/main.py
+++ Projects/Day17/main.py
@@ -1,12 +1,12 @@
 class User:
- pass
+    def __init__(self, user_id, user_name):
+        self.id = user_id
+        self.user_name = user_name
 
 
-user_1  = User()
+user_1 = User("001", "srinivas")
 
-user_1.id="001"
-user_1.name="srinivas"
+print(user_1.user_name, user_1.id)
 
-print(user_1.name)
 
-print("test commit")
+##############
