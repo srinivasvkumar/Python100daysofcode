a883adb185adadd50a2e05983b47608c63a19427
Srinivas Seedaragaddi <srinivasvkumar@gmail.com>
2023-11-02 12:34:02 +0530

    Reformatted as per standards

--- Projects/Day17/main.py
+++ Projects/Day17/main.py
@@ -2,13 +2,12 @@ class User:
     def __init__(self, user_id, user_name):
         self.id = user_id
         self.user_name = user_name
-        self.followers=0
-        self.following=0
+        self.followers = 0
+        self.following = 0
 
-    def follow(self,user):
-        user.followers+=1
-        self.following+=1
-
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("001", "srinivas")
@@ -16,12 +15,8 @@ user_2 = User("002", "sowmya")
 
 user_1.follow(user_2)
 
-print(f"{user_1.user_name} followers and following are ",user_1.followers,user_1.following)
+print(f"{user_1.user_name} followers and following are ", user_1.followers, user_1.following)
 
-print(f"{user_2.user_name} followers and following are ",user_2.followers,user_2.following)
+print(f"{user_2.user_name} followers and following are ", user_2.followers, user_2.following)
 
 ##############Methods##
-
-
-
-
