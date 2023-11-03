fe8baca53507bf60cfd88667c993510082447245
Srinivas Seedaragaddi <srinivasvkumar@gmail.com>
2023-11-03 12:33:28 +0530

    turtle square

--- Projects/Day18/main.py
+++ Projects/Day18/main.py
@@ -1,1 +1,18 @@
-print("Day 18")
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("green")
+
+
+def turle_move():
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+
+for _ in range(4):
+    turle_move()
+
+screen = Screen()
+screen.exitonclick()
