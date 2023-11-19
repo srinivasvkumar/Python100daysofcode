6b0764b7c60d0320bef3de80ffe16b9d06452e1e
srinivasvkumar <srinivasvkumar@gmail.com>
2023-11-19 21:33:50 +0530

    Day 25 - states game

---
+++ Projects/Day26/main.py
@@ -0,0 +1,7 @@
+import pandas
+
+build_dic = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+act_data = {row.letter: row.code for (index, row) in build_dic.iterrows()}
+print(build_dic)
+print(act_data)
